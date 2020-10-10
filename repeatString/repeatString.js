2a8ac192bf12a6c05104ffb1a5681cdf1d25417b
krisyw1 <krisyw1@gmail.com>
2020-10-10 16:10:55 +0100

    completed repeatString and pass spec

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(phrase, reps) {
+    let x = reps;
+    let message = '';
+    if (x < 0){
+        return 'ERROR';
+    }
+    for (;x > 0; x--){
+        message = message + phrase;
+    }
+    return message;
 }
 
 module.exports = repeatString
