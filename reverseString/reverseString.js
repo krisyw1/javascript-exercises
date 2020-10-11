c73e7bccb9998e4571ec97d5ef6bcd0db6dedc34
krisyw1 <krisyw1@gmail.com>
2020-10-11 16:31:35 +0100

    reverse string complete passed all tests

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(userString) {
+let rvrsInput = "";
+let i = userString.length - 1
+for (;i >= 0; i--){
+    rvrsInput += userString[i];
+}
+return rvrsInput;
 }
 
 module.exports = reverseString
