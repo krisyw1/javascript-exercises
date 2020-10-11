e0aa5bef6f9899298487d2dda0672140acc4606d
krisyw1 <krisyw1@gmail.com>
2020-10-11 17:03:15 +0100

    code pass-1 and test

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,9 +1,14 @@
-const removeFromArray = function() {
-//get array length
-//loop
+const removeFromArray = function(passedArray, trashValue) {
+let i = passedArray.length; 
 //array(loop) = remove then remove
+for(;i == 0; i--){
+    if(passedArray[i]) === trashValue){
+        passedArray.splice(0,i);
+    }
+}
 //end loop
 //return
+return passedArray;
 }
 
 module.exports = removeFromArray
