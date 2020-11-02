9e7698e404f8af3628f39258d736bc08dbd3812c
krisyw1 <krisyw1@gmail.com>
2020-11-02 16:45:54 +0000

    rmvarr test1 OK. add psudo for further tests

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,14 +1,18 @@
-const removeFromArray = function(passedArray, trashValue) {
+const removeFromArray = function(passedArray, ...trashValue) {
 
-let rtnArray;
-//array(loop) = remove then remove
+    //is rest array longer than 1 value
+    //if no call remove
+    //if yes loop length of trashValue
+        //call remove
+    //end
+
+
 for(let i = 0 ;i < passedArray.length ; i++){
     if(passedArray[i] == trashValue){
         passedArray.splice(i,1);
     }
 }
-//end loop
-//return
+
 return passedArray;
 
 }
