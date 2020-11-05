613e18adb0b1914c52fee0dbf77ef17e1c563af3
krisyw1 <krisyw1@gmail.com>
2020-11-05 21:04:50 +0000

    psudocode and validate input

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(x,y) {
 
+    //check input
+    if(Number.isInteger(x) && Number.isInteger(y) && x >= 0 && y >= 0){
+        console.log("i work");
+    }
+    //min and max
+    //loop for sum
+    //return
 }
 
 module.exports = sumAll
