7a7d8b70daa70f9f046dcdd6cb1bab1af6e469dc
krisyw1 <krisyw1@gmail.com>
2020-11-05 21:19:57 +0000

    write code - first draft

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,12 +1,26 @@
 const sumAll = function(x,y) {
-
-    //check input
+let maxValue;
+let minValue;
+let sumSum = 0;
+//check input and set min and max
     if(Number.isInteger(x) && Number.isInteger(y) && x >= 0 && y >= 0){
-        console.log("i work");
+        if(x > y){
+            maxValue = x;
+            minValue = y;
+        } else{
+            maxValue = y;
+            minValue = x;
+        }
+    } else{
+        return "ERROR";
     }
-    //min and max
-    //loop for sum
-    //return
+//loop for sum
+    for(i=minValue; i=maxValue; i++){
+        sumSum = sumSum + i;
+    }   
+    
+//return
+    return sumSum;
 }
 
 module.exports = sumAll
